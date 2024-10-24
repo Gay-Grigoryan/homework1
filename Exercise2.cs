42f1252398dbad1c476a774081548aea1a81b2e5
Gay-Grigoryan <159516275+Gay-Grigoryan@users.noreply.github.com>
2024-10-24 10:52:19 +0400

    Add files via upload

---
+++ Exercise2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 50, 4, 7, 3, 90 };
+            InsertionSort(arr, arr.Length);
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+        }
+
+        public static void InsertionSort(int[] arr, int n)
+        {
+
+            if (n <= 1)
+                return;
+
+            InsertionSort(arr, n - 1);
+
+            int last = arr[n - 1];
+            int i = n - 2;
+
+            while (i >= 0 && arr[i] > last)
+            {
+                arr[i + 1] = arr[i];
+                i--;
+            }
+            arr[i + 1] = last;
+        }
+    }
+}
